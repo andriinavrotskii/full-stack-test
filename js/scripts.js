d16eaa5a58c7b87d380675628d40bd95e3803936
Andrii Navrotskii <andri.navrotskii@gmail.com>
2017-06-30 00:09:56 +0300

    base table generation

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,51 @@
-var start = function() {
-    console.log('HELLO');
-}
-
-
-var ajaxRequest = function() {
-    $.get("/api/articles", function(data, status){
-        console.log(status);
-        console.log(data);
-    });
-}
-
-
 $(document).ready(function(){
     $("#start").click(function(){
-        start();
         ajaxRequest();
     });
 });
+
+
+var ajaxRequest = function() {
+    $.post("/api/articles", function(data, status){
+        dataObj = JSON.parse(data);
+        console.log(status);
+        console.log(dataObj);
+        if (status == 'success') {
+            generateTable(dataObj);
+        } else {
+            console.log('Something wrong!')
+        }
+    });
+}
+
+
+var generateTable = function(dataObj) {
+        console.log('generateTable');
+
+    let tableHeader = "<table><thead><tr><th>ID</th><th>Image</th><th>Title</th><th>Language</th><th>Status</th></tr></thead><tbody>";
+    let tableFooter = "</tbody></table>";
+
+    let tableContent = "";
+
+    dataObj.forEach(function(item, index) {
+        let content = JSON.parse(item.content);
+        
+        tableContent += 
+                "<tr><td>"
+                + item.id
+                + "</td><td>"
+                + content.title.title_image
+                + "</td><td>"
+                + content.title.title 
+                + "</td><td>" 
+                + item.lang
+                + "</td><td>"
+                + item.status
+                + "</td></tr>";
+
+    });
+
+    $("#table").html(tableHeader + tableContent + tableFooter);
+
+}
+
